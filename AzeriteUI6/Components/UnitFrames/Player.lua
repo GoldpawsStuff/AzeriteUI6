622be3c55db640fa2472da0645d2690c84574799
Lars Norberg <github@larsnorbergofficial.com>
2026-02-18 16:24:50 +0100

    *

--- AzeriteUI6/Components/UnitFrames/Player.lua
+++ AzeriteUI6/Components/UnitFrames/Player.lua
@@ -40,7 +40,118 @@ local AbbreviateNumber = ns.AbbreviateNumber
 local GetFont = ns.GetFont
 local GetMedia = ns.GetMedia
 
--- Setup the unitframe
+
+-- Toggle cast info and health info when castbar is visible.
+local Castbar_PostUpdateTexts = function(element)
+	if (element:IsShown()) then
+		element.Text:Show()
+		element.Time:Show()
+		element.__owner.Health.Value:Hide()
+	else
+		element.Text:Hide()
+		element.Time:Hide()
+		element.__owner.Health.Value:Show()
+	end
+end
+
+-- Toggle cast text color on protected casts.
+local Castbar_PostCastInterruptible = function(element, unit)
+	if (element.notInterruptible) then
+		element.Text:SetTextColor(229/255, 178/255, 38/255, .75)
+	else
+		element.Text:SetTextColor(250/255, 250/255, 250/255, .5)
+	end
+end
+
+-- Primarily called to store the max value for the value update.
+local Health_OnMinMaxChanged = function(element, min, max) 
+	element.min, element.max = min, max
+end
+
+-- This will be called when the value changes, 
+-- and it's allowed access to min/max/cur values of the bar.
+-- This script handler is one of the only ones that are allowed to do that in WoW12.
+local Health_OnValueChanged = function(element, val) 
+	if (val and element.max and element.max > 0) then
+		if (UnitIsDeadOrGhost(element.__owner.unit)) then
+			element.Value:SetText(DEAD)
+		else
+			element.Value:SetText(AbbreviateNumber(val))
+		end 
+	else
+		element.Value:SetText("")
+	end
+end
+
+-- Primarily called to store the max value for the value update.
+local Power_OnMinMaxChanged = function(element, min, max) 
+	element.min, element.max = min, max
+end
+
+-- This will be called when the value changes, 
+-- and it's allowed access and do math to min/max/cur values of the bar.
+-- This script handler is one of the only ones that are allowed to do that in WoW12.
+local Power_OnValueChanged = function(element, val) 
+	if (val and element.max and element.max > 0) then
+		if (not UnitIsDeadOrGhost(element.__owner.unit)) then
+			element.Value:SetText(AbbreviateNumber(val))
+		else
+			element.Value:SetText("")
+		end
+		element.Texture:SetTexCoord(50/255, 206/255, 37/255 + (1 - val/element.max)*((219-37)/255), 219/255)
+		element.Texture:SetPoint("TOP", 0, (- (element.max-val)/element.max * 140))
+	end
+end
+
+-- Partly sourced from oUF's power element's coloring function
+local Power_UpdateColor = function(self, event, unit)
+	if (self.unit ~= unit) then return end
+	local element = self.Power
+
+	if (db.useIceCrystal) then
+		element.Texture:SetVertexColor(1, 1, 1) 
+	else
+		local r, g, b, color
+		if (element.colorPower) then
+			if (not color) then
+				local pType, pToken, altR, altG, altB = UnitPowerType(unit)
+				color = self.colors.power[pToken]
+
+				if (not color and altR) then
+					r, g, b = altR, altG, altB
+					if (r > 1 or g > 1 or b > 1) then
+						-- BUG: As of 7.0.3, altR, altG, altB may be in 0-1 or 0-255 range.
+						r, g, b = r / 255, g / 255, b / 255
+					end
+				else
+					color = self.colors.power[pToken.."_CRYSTAL"] or self.colors.power[pType] or self.colors.power.MANA
+				end
+			end
+		end
+
+		-- it's done this way so that only non-standard powers have r, g, b values
+		if (b) then
+			element.Texture:SetVertexColor(r, g, b)
+		elseif(color) then
+			element.Texture:SetVertexColor(color:GetRGB())
+		end
+	end
+
+	--[[ Callback: Power:PostUpdateColor(unit, color, altR, altG, altB)
+	Called after the element color has been updated.
+
+	* self  - the Power element
+	* unit  - the unit for which the update has been triggered (string)
+	* color - the used ColorMixin-based object (table?)
+	* altR  - the red component of the used alternative color (number?)[0-1]
+	* altG  - the green component of the used alternative color (number?)[0-1]
+	* altB  - the blue component of the used alternative color (number?)[0-1]
+	--]]
+	if (element.PostUpdateColor) then
+		element:PostUpdateColor(unit, color, r, g, b)
+	end
+end
+
 local style = function(self, unit)
 
 	-- General frame settings
@@ -80,25 +191,8 @@ local style = function(self, unit)
 	healthValue:SetJustifyH("LEFT")
 	healthValue:SetJustifyV("MIDDLE")
 
-	local hMin, hMax
-	health:SetScript("OnMinMaxChanged", function(self, min, max) 
-		hMin, hMax = min, max
-	end)
-
-	-- This will be called when the value changes, 
-	-- and it's allowed access to min/max/cur values of the bar.
-	-- This script handler is one of the only ones that are allowed to do that in WoW12.
-	health:SetScript("OnValueChanged", function(self, val) 
-		if (val and hMax and hMax > 0) then
-			if (UnitIsDeadOrGhost(unit)) then
-				healthValue:SetText(DEAD)
-			else
-				healthValue:SetText(AbbreviateNumber(val))
-			end 
-		else
-			healthValue:SetText("")
-		end
-	end)
+	health:SetScript("OnMinMaxChanged", Health_OnMinMaxChanged)
+	health:SetScript("OnValueChanged", Health_OnValueChanged)
 
 	-- Options
 	health.colorTapping = false
@@ -234,28 +328,6 @@ local style = function(self, unit)
 	castbarTime:SetJustifyV("MIDDLE")
 	castbarTime:Hide()
 
-	-- Toggle cast info and health info when castbar is visible.
-	local Castbar_PostUpdateTexts = function(element)
-		if (element:IsShown()) then
-			element.Text:Show()
-			element.Time:Show()
-			element.__owner.Health.Value:Hide()
-		else
-			element.Text:Hide()
-			element.Time:Hide()
-			element.__owner.Health.Value:Show()
-		end
-	end
-
-	-- Toggle cast text color on protected casts.
-	local Castbar_PostCastInterruptible = function(element, unit)
-		if (element.notInterruptible) then
-			element.Text:SetTextColor(229/255, 178/255, 38/255, .75)
-		else
-			element.Text:SetTextColor(250/255, 250/255, 250/255, .5)
-		end
-	end
-
 	-- Attach scripts
 	castbar:HookScript("OnShow", Castbar_PostUpdateTexts)
 	castbar:HookScript("OnHide", Castbar_PostUpdateTexts)
@@ -279,7 +351,7 @@ local style = function(self, unit)
 	local powerBg = power:CreateTexture(nil, "BACKGROUND", nil, -7)
 	powerBg:SetSize(196, 196)
 	powerBg:SetPoint("CENTER", 0, 0)
-	powerBg:SetTexture(db().useIceCrystal and GetMedia("power-crystal-ice-back") or GetMedia("power_crystal_back"))
+	powerBg:SetTexture(db.useIceCrystal and GetMedia("power-crystal-ice-back") or GetMedia("power_crystal_back"))
 
 	-- Fake powerbar texture, needed for our vertical bars
 	local powerTex = power:CreateTexture(nil, "BACKGROUND", nil, -6)
@@ -288,7 +360,7 @@ local style = function(self, unit)
 	powerTex:SetPoint("RIGHT", 0, 0)
 	powerTex:SetPoint("TOP", 0, 0)
 
-	if (db().useIceCrystal) then
+	if (db.useIceCrystal) then
 		powerTex:SetTexture(GetMedia("power-crystal-ice-front")) 
 		powerTex:SetVertexColor(1, 1, 1) 
 		powerTex:SetTexCoord(50/255, 206/255, 37/255, 219/255)
@@ -306,27 +378,6 @@ local style = function(self, unit)
 	powerValue:SetJustifyH("CENTER")
 	powerValue:SetJustifyV("MIDDLE")
 
-	local pMin, pMax
-	power:SetScript("OnMinMaxChanged", function(self, min, max) 
-		pMin, pMax = min, max
-		-- Do we actually need to do anything more here? 
-	end)
-
-	-- This will be called when the value changes, 
-	-- and it's allowed access and do math to min/max/cur values of the bar.
-	-- This script handler is one of the only ones that are allowed to do that in WoW12.
-	power:SetScript("OnValueChanged", function(self, val) 
-		if (val and pMax and pMax > 0) then
-			if (not UnitIsDeadOrGhost(unit)) then
-				powerValue:SetText(AbbreviateNumber(val))
-			else
-				powerValue:SetText("")
-			end
-			powerTex:SetTexCoord(50/255, 206/255, 37/255 + (1 - val/pMax)*((219-37)/255), 219/255)
-			powerTex:SetPoint("TOP", 0, (- (pMax-val)/pMax * 140))
-		end
-	end)
-
 	-- Power foreground. The "case" of the power crystal.
 	local powerFg = power:CreateTexture(nil, "BACKGROUND", nil, -5)
 	powerFg:SetSize(198,98)
@@ -334,55 +385,9 @@ local style = function(self, unit)
 	powerFg:SetTexture(GetMedia("pw_crystal_case"))
 	powerFg:SetVertexColor(192/255, 192/255, 192/255)
 
-	-- Partly sourced from oUF's power element's coloring function
-	local Power_UpdateColor = function(self, event, unit)
-		if (self.unit ~= unit) then return end
-		local element = self.Power
-
-		if (db().useIceCrystal) then
-			powerTex:SetVertexColor(1, 1, 1) 
-		else
-			local r, g, b, color
-			if (element.colorPower) then
-				if (not color) then
-					local pType, pToken, altR, altG, altB = UnitPowerType(unit)
-					color = self.colors.power[pToken]
-
-					if (not color and altR) then
-						r, g, b = altR, altG, altB
-						if (r > 1 or g > 1 or b > 1) then
-							-- BUG: As of 7.0.3, altR, altG, altB may be in 0-1 or 0-255 range.
-							r, g, b = r / 255, g / 255, b / 255
-						end
-					else
-						color = self.colors.power[pToken.."_CRYSTAL"] or self.colors.power[pType] or self.colors.power.MANA
-					end
-				end
-			end
-
-			-- it's done this way so that only non-standard powers have r, g, b values
-			if (b) then
-				powerTex:SetVertexColor(r, g, b)
-			elseif(color) then
-				powerTex:SetVertexColor(color:GetRGB())
-			end
-		end
-
-		--[[ Callback: Power:PostUpdateColor(unit, color, altR, altG, altB)
-		Called after the element color has been updated.
-
-		* self  - the Power element
-		* unit  - the unit for which the update has been triggered (string)
-		* color - the used ColorMixin-based object (table?)
-		* altR  - the red component of the used alternative color (number?)[0-1]
-		* altG  - the green component of the used alternative color (number?)[0-1]
-		* altB  - the blue component of the used alternative color (number?)[0-1]
-		--]]
-		if (element.PostUpdateColor) then
-			element:PostUpdateColor(unit, color, r, g, b)
-		end
-	end
-	power.UpdateColor = Power_UpdateColor
+	-- Attach scripts
+	power:SetScript("OnMinMaxChanged", Power_OnMinMaxChanged)
+	power:SetScript("OnValueChanged", Power_OnValueChanged)
 
 	-- Options
 	power.colorPower = true -- false -- true to follow default coloring, false to never/manually modify
@@ -392,6 +397,8 @@ local style = function(self, unit)
 	-- Register it with oUF
 	self.Power = power
 	self.Power.Value = powerValue
+	self.Power.Texture = powerTex
+	self.Power.UpdateColor = Power_UpdateColor
 
 end
 
@@ -413,14 +420,12 @@ Player.OnInitialize = function(self)
 	--self.db.RegisterCallback(self, "OnProfileCopied", "RefreshConfig")
 	--self.db.RegisterCallback(self, "OnProfileReset", "RefreshConfig")
 
-	-- Utility function to get saved settings or defaults
+	-- Utility to get saved settings or defaults
 	-- *Will default to defaults if the saved settings above don't exist (during development)
-	db = function(forceDefaults)
-		if (forceDefaults) then 
-			return defaults.profile
-		end
+	db = (function(forceDefaults)
+		if (forceDefaults) then return defaults.profile end
 		return self.db and self.db.profile or defaults.profile
-	end
+	end)(false)
 end
 
 Player.OnEnable = function(self)
